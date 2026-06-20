f88239f0e89de81d8edafcaad306cfc152097e57
albert379v2 <oscar.diaz.rivera379@gmail.com>
2026-06-19 23:31:41 -0700

    Add logging for MyApplication class

--- app/src/main/java/com/sheinmulti/profile/MyApplication.kt
+++ app/src/main/java/com/sheinmulti/profile/MyApplication.kt
@@ -2,12 +2,19 @@ package com.sheinmulti.profile
 
 import android.app.Application
 import android.webkit.WebView
+import android.util.Log
 
 class MyApplication : Application() {
+    companion object {
+        private const val TAG = "MyApplication"
+    }
+
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
             WebView.setWebContentsDebuggingEnabled(true)
         }
+        // Data directory suffix must be set BEFORE any WebView is instantiated
+        // This is handled per-profile in WebViewActivity with try-catch
     }
 }
